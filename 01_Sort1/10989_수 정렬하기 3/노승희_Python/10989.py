f2241f496e174dc97ba6ecaa052d785af2dfc5d2
seung-heee <tmdgmldi2595@naver.com>
2024-07-01 17:23:39 +0900

    Create 10989.py, 2750.py, 2751.py

---
+++ 01_Sort1/10989_수 정렬하기 3/노승희_Python/10989.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+arr = [0] * 10001
+
+# 입력받은 숫자의 빈도 증가
+for _ in range(N):
+  num = int(input())
+  arr[num] += 1
+
+# 배열을 순회하며 빈도 수만큼 숫자 출력
+for i in range(10001):
+  if arr[i] != 0:
+    for j in range(arr[i]):
+      print(i)
+
+# 카운팅 정렬 > 입력데이터 범위가 한정된 경우에 효율적
---
+++ 01_Sort1/2750_수 정렬하기 1/노승희_Python/2750.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+N = int(input())
+arr = []
+
+for i in range(N):
+  arr.append(int(input()))
+
+# 퀵 정렬
+def quick_sort(arr):
+  if len(arr) <= 1:
+    return arr
+  
+  pivot = arr[0]
+  tail = arr[1:]
+
+  left = [x for x in tail if x < pivot]
+  right = [x for x in tail if x > pivot]
+
+  return quick_sort(left) + [pivot] + quick_sort(right)
+
+new_arr = quick_sort(arr)
+
+for i in new_arr:
+  print(i)
