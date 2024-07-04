273cb99b11f80934b887500ae0e94e91b62983f3
SUBIN <134045937+subinsong01@users.noreply.github.com>
2024-07-04 14:34:03 +0900

    Update 1181.py

--- 02_Sort2/1181_단어정렬/송수빈_Python/1181.py
+++ 02_Sort2/1181_단어정렬/송수빈_Python/1181.py
@@ -1,12 +1,14 @@
-# 언어 : Python , (성공/실패) : 1/6 , 메모리 : 35732 KB , 시간 : 92ms
-
 import sys
-
 n = int(sys.stdin.readline())
-words = [sys.stdin.readline().strip() for _ in range(n)]
 
-words = list(set(words))
-words.sort(key=lambda word: (len(word), word))
+word = []
+for i in range(n):
+    word.append(sys.stdin.readline().strip()) #strip으로 공백제거 
 
-for word in words:
-    print(word)
+word = list(set(word)) #set = 중복제거 
+
+word.sort() #단어순 정렬
+word.sort(key=len) #길이순 정렬 
+
+for i in word:
+    print(i)
