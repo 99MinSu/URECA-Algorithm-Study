e719b4c332f9a19ab4ecb347ca62bc1cf0bd7cc1
SUBIN <134045937+subinsong01@users.noreply.github.com>
2024-07-08 19:27:26 +0900

    Update 10814.py

--- 02_Sort2/10814_나이순 정렬/송수빈_Python/10814.py
+++ 02_Sort2/10814_나이순 정렬/송수빈_Python/10814.py
@@ -2,7 +2,7 @@ N = int(input()) #회원 수
 info = [] #회원들 정보
 
 for _ in range(N):
-    age, name = intput().split() #나이 이름 받기
+    age, name = input().split() #나이 이름 받기
     info.append([int(age), name])
 
 info.sort(key=lambda x:int(x[0])) #나이 기준으로 정렬 
