b7e26c913dc984cfe7a30dfacfb4dd9d8d6b6f94
seung-heee <tmdgmldi2595@naver.com>
2024-07-23 18:44:20 +0900

    DP 코드 수정 & 설명 추가

--- 07_동적 프로그래밍1/02_1010_다리놓기/노승희.py
+++ 07_동적 프로그래밍1/02_1010_다리놓기/노승희.py
@@ -1,15 +1,18 @@
 # 언어 : Python , (성공/실패) : 1/0, 메모리 : 31120 KB , 시간 : 40 ms
-import sys
-t = int(sys.stdin.readline())
+def factorial(n):
+    num = 1
+    for i in range(1, n+1):
+        num *= i
+    return num
 
-for i in range(t):
-    n, m = map(int, sys.stdin.readline().split())
-    if n == m:
-        print(1)
-    else:
-        ans = 1
-        for i in range(m, m-n, -1):
-            ans *= i
-        for j in range(n, 1, -1):
-            ans = ans // j
-        print(ans)
+T = int(input())
+
+for _ in range(T):
+    n, m = map(int, input().split())
+    bridge = factorial(m) // (factorial(n) * factorial(m - n))
+    print(bridge)
+
+# 조합 = 서로 다른 m개 중에 n개를 선택하는 경우의 수
+# m이 n보다 크기 때문에 최대 연결할 수 있는 다리의 개수는 n개
+# m개의 지역에 n개의 다리를 놓을 수 있는 경우의 수를 구하는 것이기 때문에
+# mCn 으로 표현할 수 있고 이는 m! 을 (m-n)!n! 으로 나눈 값이 됨.
--- 07_동적 프로그래밍1/03_9655_돌 게임/노승희.py
+++ 07_동적 프로그래밍1/03_9655_돌 게임/노승희.py
@@ -7,15 +7,17 @@ dp[1] = 1 # SK
 dp[2] = 0 # CY
 dp[3] = 1 # SK
 
-
 for i in range(4, n+1):
-    if dp[i-1] != 1 or dp[i-3] != 1: 
-        dp[i] = 1
-    else:
+    if dp[i-1] == 1 or dp[i-3] ==1: 
         dp[i] = 0
+    else:
+        dp[i] = 1
 
 if dp[n] == 1:
     print("SK")
 
 else:
     print("CY")
+
+# 1이면 상근이가 이김
+# 0이면 창영이가 이김
