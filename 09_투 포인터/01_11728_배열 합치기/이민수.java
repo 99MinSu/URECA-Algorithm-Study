4a11be388a24ef76a7ef61edbd7a7336808c1991
이민수 <89891084+99MinSu@users.noreply.github.com>
2024-08-06 17:24:43 +0900

    Update 이민수.java

--- 09_투 포인터/01_11728_배열 합치기/이민수.java
+++ 09_투 포인터/01_11728_배열 합치기/이민수.java
@@ -4,34 +4,51 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+			
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		int[] arr = new int[N];
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int [] a = new int[n];
+		int [] b = new int[m];
 		
 		st = new StringTokenizer(br.readLine());
 		
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < n; i++) {
+			a[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int ans = 0;
-		int start = 0;
-		int end = 0;
-		int cnt[] = new int[100001];
-		while(end < N) {
-			while(end < N && cnt[arr[end]] + 1 <= K) {
-				cnt[arr[end]]++;
-				end++;
-			}
-			int len = end-start;
-			ans = Math.max(ans, len);
-			cnt[arr[start]]--;
-			start++;
+		st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < m; i++) {
+			b[i] = Integer.parseInt(st.nextToken());
 		}
-		System.out.println(ans);
+		
+		int p1 = 0, p2=0;
+		
+		while(p1<n && p2<m) {  
+			if(a[p1]<= b[p2]) {
+				sb.append(a[p1++]+" ");
+			}
+			else {
+				sb.append(b[p2++]+" ");
+			}
+		}
+
+		if(p1==n) {
+			for (int i = p2; i < m; i++) {
+				sb.append(b[i]+" ");
+			}
+		}
+		if(p2==m) {
+			for (int i = p1; i < n; i++) {
+				sb.append(a[i]+" ");
+			}
+		}
+		System.out.println(sb);
+        
     }
 }
-// 언어 : JAVA , (성공/실패) : 1/0 , 메모리 : 32996 KB , 시간 : 312 ms
+// 언어 : JAVA , (성공/실패) : 1/0 , 메모리 : 318816 KB , 시간 : 1464 ms
