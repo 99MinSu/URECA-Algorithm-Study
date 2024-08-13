c2247ea1ce8ade29934b08acbad096d43d291d04
이민수 <89891084+99MinSu@users.noreply.github.com>
2024-08-13 19:43:53 +0900

    Create 이민수.java

---
+++ 10_완전 탐색/03_1025_제곱수 찾기/이민수.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static int N, M;
+	public static int[][] map;
+	public static String S, T;
+	public static int result = -1;
+    public static void main(String[] args) throws Exception{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	N = Integer.parseInt(st.nextToken());
+    	M = Integer.parseInt(st.nextToken());
+    	
+    	map = new int[N][M];
+    	for(int i=0;i<N;i++) {
+    		String s = br.readLine();
+    		for(int j=0;j<M;j++) {
+    			map[i][j] = s.charAt(j) - '0';	
+    		}
+    	}
+    	
+    	for(int i=0;i<N;i++) {
+    		for(int j=0;j<M;j++) {
+    			for(int di =-N; di<N;di++){ //행의 등차값 ( 행의 등차값은 -N ~ +N 까지 ) 
+    				for(int dj = -M; dj<M;dj++) { //열의 등차값 ( 열의 등차값은 -M ~ +M 까지 )
+    					
+    					if(di == 0 && dj == 0) { //둘다 움직이지 않을떄
+    						continue;
+    					}
+    					
+    					int nI = i;
+    					int nJ = j;
+    					int now = 0;
+    					while( nI >= 0 && nI < N && nJ >= 0 && nJ < M) {
+    						//자리수 10의 자리수 씩 체크하기 위해 로직처리
+    						now *= 10;
+    						now += map[nI][nJ];
+    						
+    						//제곱근인지 판별
+    						int sqrt_check = (int) Math.sqrt( (double) now);
+    						if( sqrt_check * sqrt_check == now) result = Math.max(result, now);
+    						
+    						nI += di;
+    						nJ += dj;
+    					}
+    					
+    				}
+    			}
+    		}
+    	}
+    	System.out.println(result);
+    	
+    }   
+    
+}
+// 언어 : JAVA , (성공/실패) : 1/0 , 메모리 : 14232 KB , 시간 : 128 ms
